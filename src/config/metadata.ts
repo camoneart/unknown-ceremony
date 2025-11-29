e750e5dde443ad1e653bc59783d08b42efe9e33b
camone <106678583+camoneart@users.noreply.github.com>
2025-11-29 21:18:47 +0900

    add: metadata.ts

---
+++ src/config/metadata.ts
@@ -0,0 +1,37 @@
+import type { Metadata } from 'next';
+
+export const siteConfig = {
+  name: 'UNKNOWN CEREMONY',
+  description: 'UNKNOWN CEREMONY - 無名のための式典',
+};
+
+export const pageMetadata: Record<string, Metadata> = {
+  home: {
+    title: 'UNKNOWN CEREMONY - 無名のための式典',
+    description: siteConfig.description,
+    robots: {
+      index: false,
+      follow: false,
+      nocache: true,
+      googleBot: {
+        index: false,
+        follow: false,
+        noimageindex: true,
+      },
+    },
+  },
+  about: {
+    title: `ABOUT – ${siteConfig.name}`,
+    description: 'UNKNOWN CEREMONYについて。',
+    robots: {
+      index: false,
+      follow: false,
+      nocache: true,
+      googleBot: {
+        index: false,
+        follow: false,
+        noimageindex: true,
+      },
+    },
+  },
+};
