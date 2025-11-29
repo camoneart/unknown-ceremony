b1879144a421f72d2373dee54a885e28f344e076
camone <106678583+camoneart@users.noreply.github.com>
2025-11-29 23:28:15 +0900

    chore: add eslint-plugin-import-access

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,11 +2,26 @@ import { defineConfig, globalIgnores } from 'eslint/config';
 import nextVitals from 'eslint-config-next/core-web-vitals';
 import nextTs from 'eslint-config-next/typescript';
 import prettierConfig from 'eslint-config-prettier';
+import importAccess from 'eslint-plugin-import-access/flat-config';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
   prettierConfig,
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+    plugins: {
+      'import-access': importAccess,
+    },
+    rules: {
+      'import-access/jsdoc': ['error'],
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
