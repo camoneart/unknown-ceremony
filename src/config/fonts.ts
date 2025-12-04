504d17e664010848965956c152ca3d5c615dcbd8
camone <106678583+camoneart@users.noreply.github.com>
2025-12-05 06:10:27 +0900

    add: Font zenOldMincho

---
+++ src/config/fonts.ts
@@ -0,0 +1,8 @@
+import { Zen_Old_Mincho } from 'next/font/google';
+
+export const zenOldMincho = Zen_Old_Mincho({
+  weight: ['400', '500', '600', '700', '900'],
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-zen-old-mincho',
+});
