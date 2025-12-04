1d6bb0dd6fcb10f43b584c9a4f09ceafb5589fa8
camone <106678583+camoneart@users.noreply.github.com>
2025-12-05 05:58:58 +0900

    update: About page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,33 @@
 const AboutPage = () => {
-  return <div>AboutPage</div>;
+  return (
+    <div
+      className="h-full flex flex-col items-center justify-center text-center gap-[35px] pb-[38px]"
+      style={{ textShadow: '0 0 10px rgba(255, 255, 255, 0.5)' }}
+    >
+      <section className="max-w-[800px] grid gap-[25px]">
+        <h2 className="text-[30px] tracking-[0.2em]">CONCEPT</h2>
+        <div className="grid gap-[25px]">
+          <p className="text-[18px] font-bold leading-relaxed">
+            &quot;無名が制作した作品をWebという大きな会場にて披露する式典&quot;
+          </p>
+          <div className="leading-[1.75] grid gap-[10px]">
+            <p>有名無名は関係ない。無名が存在するから有名も存在する。</p>
+            <p>無名な私が制作した作品もまた無名。</p>
+            <p>それでもその作品一つ一つは私にとっては &quot;主役&quot; だったりするのです。</p>
+            <p>だからこそ、作品一つ一つに &quot;式&quot; を挙げてやりたい。</p>
+            <p>そんな想いを込め、&quot;UNKNOWN CEREMONY&quot; と名付けました。</p>
+          </div>
+        </div>
+      </section>
+
+      <section className="max-w-[800px] grid gap-[25px]">
+        <h2 className="text-[30px] tracking-[0.2em]">ABOUT</h2>
+        <p className="leading-[1.75]">
+          ただただひたすらモノづくりが好きな私が制作した作品を披露する場となっています。
+        </p>
+      </section>
+    </div>
+  );
 };
 
 export default AboutPage;
