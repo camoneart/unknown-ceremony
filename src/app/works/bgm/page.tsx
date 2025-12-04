66f1b220e594ce3a26d3bd3ec828e5195671481e
camone <106678583+camoneart@users.noreply.github.com>
2025-12-05 06:09:46 +0900

    add: BgmPage

---
+++ src/app/works/bgm/page.tsx
@@ -0,0 +1,9 @@
+const BgmPage = () => {
+  return (
+    <div className="grid h-full place-items-center">
+      <p className="text-[20px]">Coming soon...</p>
+    </div>
+  );
+};
+
+export default BgmPage;
