e0733414669b2f41341580251ce5c12c9187d7b4
camone <106678583+camoneart@users.noreply.github.com>
2025-12-06 12:54:05 +0900

    style: スクロール時にヘッダーの背景をぼかすようにした

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -2,14 +2,15 @@
 
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
+
 import Gnav from '@/components/layout/gnav/Gnav';
-import * as ThemeToggle from '@/features/theme-toggle/components';
 
 const Header = () => {
   const pathname = usePathname();
   const isTopPage = pathname === '/';
   const HeadingTag = isTopPage ? 'h1' : 'div';
+
   const [isScrolled, setIsScrolled] = useState(false);
 
   useEffect(() => {
@@ -18,27 +19,36 @@ const Header = () => {
     };
 
     window.addEventListener('scroll', handleScroll);
+    handleScroll(); // 初期状態のチェック
+
     return () => window.removeEventListener('scroll', handleScroll);
   }, []);
 
+  if (isTopPage) return null;
+
   return (
     <header
-      className={`bg-background sticky top-0 z-10 h-[60px] px-[30px] transition-shadow duration-200 select-none ${
-        isScrolled ? 'shadow-md' : ''
+      className={`sticky top-0 z-10 grid h-auto place-items-center px-[30px] transition-all duration-300 select-none ${
+        isScrolled ? 'bg-black/30 backdrop-blur-sm' : ''
       }`}
     >
-      <div className="flex h-full items-center justify-between">
-        <HeadingTag className="-ml-[30px] h-full text-sm font-medium tracking-widest">
-          <Link href="/" className="grid h-full cursor-pointer place-items-center px-[30px]">
+      <div className="flex w-full flex-col items-center justify-center gap-[10px] py-[15px]">
+        <HeadingTag className="text-[28px] tracking-[0.2em]">
+          <Link
+            href="/"
+            className="grid place-items-center"
+            style={{ textShadow: '0 0 10px rgba(255, 255, 255, 0.5)' }}
+          >
             UNKNOWN CEREMONY
           </Link>
         </HeadingTag>
-        <div className="flex h-full flex-1 items-center gap-4">
-          <Gnav />
-        </div>
-        <div className="ml-[12.5px]">
-          <ThemeToggle.ThemeToggle />
-        </div>
+
+        <Gnav
+          className="flex items-center gap-[15px]"
+          ulClassName="flex items-center gap-[30px]"
+          itemClassName=""
+          linkClassName=""
+        />
       </div>
     </header>
   );
