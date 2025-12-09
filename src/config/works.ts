5db677811cbfaf3b3483fc5f712ff7c5c99add45
camone <106678583+camoneart@users.noreply.github.com>
2025-12-09 21:15:53 +0900

    add: worksLinks data

---
+++ src/config/works.ts
@@ -0,0 +1,5 @@
+export const worksLinks = [
+  { href: '/works/bgm', label: 'BGM', description: '音楽' },
+  { href: '/works/movies', label: 'MOVIES', description: '映像' },
+  { href: '/works/artworks', label: 'ARTWORKS', description: 'イラスト' },
+];
