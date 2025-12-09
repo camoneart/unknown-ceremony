d8c75585c8cbf636ebe2a663a1b074cba11a17d1
camone <106678583+camoneart@users.noreply.github.com>
2025-12-09 21:11:00 +0900

    update: BgmPage

--- src/app/works/bgm/page.tsx
+++ src/app/works/bgm/page.tsx
@@ -1,7 +1,13 @@
+import { pageMetadata } from '@/config/metadata';
+
+export const metadata = pageMetadata.bgm;
+
 const BgmPage = () => {
   return (
     <div className="grid h-full place-items-center">
-      <p className="text-[20px]">Coming soon...</p>
+      <p className="text-[20px]" style={{ textShadow: '0 0 10px rgba(255, 255, 255, 0.5)' }}>
+        Coming soon...
+      </p>
     </div>
   );
 };
